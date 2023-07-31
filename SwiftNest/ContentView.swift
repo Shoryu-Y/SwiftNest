c1a7523265f9086c20517799b74a562b344807b7
Shoryu Yamamoto <tatsubee@pixiv.co.jp>
2023-07-31 22:46:36 +0900

    Initial Commit

---
+++ SwiftNest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftNest
+//
+//  Created by tatsubee on 2023/07/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftNest/SwiftNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNestApp.swift
+//  SwiftNest
+//
+//  Created by tatsubee on 2023/07/31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
