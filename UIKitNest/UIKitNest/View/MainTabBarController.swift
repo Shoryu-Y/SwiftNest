1cedc57a9e7e055d6de66c9052cf228594da8032
tatsubee <tatsubee@pixiv.co.jp>
2025-11-05 23:17:16 +0900

    add UIKitNest

---
+++ UIKitNest/UIKitNest/View/MainTabBarController.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class MainTabBarController: UITabBarController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		let homeViewController = HomeViewController()
+		homeViewController.title = "ホーム"
+		homeViewController.tabBarItem = UITabBarItem(title: "ホーム", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
+
+		let settingsViewController = SettingsViewController()
+		settingsViewController.title = "設定"
+		settingsViewController.tabBarItem = UITabBarItem(title: "設定", image: UIImage(systemName: "gearshape"), selectedImage: UIImage(systemName: "gearshape.fill"))
+
+		viewControllers = [
+			UINavigationController(rootViewController: homeViewController),
+			UINavigationController(rootViewController: settingsViewController)
+		]
+	}
+}
+
+
